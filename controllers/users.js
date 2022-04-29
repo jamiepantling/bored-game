e93cf5bdc03c82c3f6ecffa2664641502c468b14
Jamie Pantling <99222790+jamiepantling@users.noreply.github.com>
2022-04-29 19:00:15 -0400

    init, add oauth

---
+++ controllers/users.js
@@ -0,0 +1,16 @@
+let User = require("../models/user");
+
+module.exports = {
+    index
+}
+
+function index(req, res, next) {
+    console.log(req.user)
+      res.render('index' ,
+      {
+        user: req.user,
+        title: "Hi"
+        }
+        );
+    };
+  
---
+++ routes/index.js
@@ -0,0 +1,30 @@
+var express = require('express');
+var router = express.Router();
+const passport = require('passport');
+const usersCtrl = require("../controllers/users")
+
+/* GET home page. */
+router.get('/', usersCtrl.index);
+
+router.get('/auth/google', passport.authenticate(
+  'google',
+  // More needed here?
+  { scope: ['profile', 'email'] }
+));
+
+// Google OAuth callback route
+router.get('/oauth2callback', passport.authenticate(
+  'google',
+  {
+    // Change redirect?
+    successRedirect : '/',
+    failureRedirect : '/'
+  }
+));
+
+router.get('/logout', function(req, res){
+  req.logout();
+  res.redirect('/');
+});
+
+module.exports = router;
