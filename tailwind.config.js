86cdd77f3154382a8ab6758fb9ef5117b61f4e2b
Jamie Pantling <99222790+jamiepantling@users.noreply.github.com>
2022-08-08 18:49:10 -0400

    fix font, responsive

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,14 @@
 module.exports = {
   content: ["./views/**/*.ejs"],
   theme: {
-    extend: {backgroundImage: (theme) => ({
-      quacks: "url('../public/images/quacks.png')",
-    }),},
+    extend: {
+      backgroundImage: (theme) => ({
+        quacks: "url('../public/images/quacks.png')",
+      }),
+      fontFamily: {
+        'title': ['"Architects Daughter"', 'cursive']
+      }
+    },
   },
   plugins: [
     require('@tailwindcss/forms')
